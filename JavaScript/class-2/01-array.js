32716ceb03e9ed2d467718c77f155dc51141def9
Biswajit1824 <biswajitsahu1824@gmail.com>
2024-05-22 18:16:16 +0530

    new file added

--- JavaScript/class-2/01-array.js
+++ JavaScript/class-2/01-array.js
@@ -14,25 +14,25 @@ let num3 = 100;
 //Now
 
 let myArray = [num1, num2, num3]
-console.log(myArray);
+// console.log(myArray);
 
 let mixedArray = ["Biswajit", "Silan",24,54];
-console.log(mixedArray);
+// console.log(mixedArray);
 
 // Accessing elements :
 
-console.log(mixedArray.length); 
-console.log(mixedArray[1]);
+// console.log(mixedArray.length); 
+// console.log(mixedArray[1]);
 
 // in optimized way
 
 // Print me the last value of randomArray
-console.log(mixedArray[3]);
-console.log(mixedArray[mixedArray.length - 1]);
+// console.log(mixedArray[3]);
+// console.log(mixedArray[mixedArray.length - 1]);
 
 //Modifying the element :
 mixedArray[2] = 19;
-console.log(mixedArray);
+// console.log(mixedArray);
 
 //Array methods :
 
@@ -40,16 +40,70 @@ console.log(mixedArray);
 // The push() method adds one or more elements to the end of an array and returns the new length of the array.
 
 let bikes = ["Pulsar", "Ninja", "KTM", "Harley", "Triumph"];
-console.log(bikes);
-console.log(bikes.length);
+// console.log(bikes);
+// console.log(bikes.length);
 
 bikes.push("hero");
-console.log(bikes);
-console.log(bikes.length);
+// console.log(bikes);
+// console.log(bikes.length);
 
 // Pop method
 
-console.log(bikes.pop());
-console.log(bikes.pop());
+// console.log(bikes.pop());
+// console.log(bikes.pop());
+// console.log(bikes);
+
+// Unshift method :
+// The unshift() method adds one or more elements to the beginning of an array and returns the new length of the array.
+
+// bikes.unshift("Yamaha"); // Yamaha
+// console.log(bikes);
+
+// Shift method :
+// The shift() method removes the first element from an array and returns that element. This method changes the length of the array.
+
+console.log(bikes.shift()); // Yamaha
 console.log(bikes);
 
+// Slice method :
+// The slice() method extracts a part of an array and returns the extracted part in a new array.
+
+// console.log(bikes.slice(1, 3)); // Output : ["Ninja", "KTM"]
+// console.log(bikes.slice(0, 4)); // 1st index will include and last index won't include
+
+
+// Splice method :
+// The splice() method adds/removes items to/from an array, and returns the removed item(s).
+
+// console.log(bikes.splice(0,4));  // start: the index at which you need to change the array
+// deleteCount : The numbers you want to delete/extracts
+// end: the index at which you need to stop changing the array
+
+// console.log(bikes.splice(0, 3));
+// console.log(bikes.splice(1, 4));
+
+// Adding element using splice method :
+
+// bikes.splice(1, 0, "Kawasaki","Honda"); // 1: start from index 1,  0: delete no
+// console.log(bikes);
+
+// Replace using splice method :
+// bikes.splice(1, 2, "Kawasaki", "Honda"); // 1: start from index 1,  2: delete no
+// console.log(bikes);
+
+// concat method :
+// The concat() method is used to merge two or more arrays. This method does not change the existing arrays, but returns a new array.
+
+// let cars = ["Ferrari", "BMW", "Audi", "Porsche", "Range-Rover"];
+// let vehicle = bikes.concat(cars);
+// console.log(vehicle);
+
+// console.log(Array.isArray(cars)); // to check the type
+console.log(Array.from("Biswajit")); // to create an array
+
+// Flat array method :
+// The flat() method creates a new array with all sub-array elements concatenated into it recursively up to the specified depth.
+
+let mixedArray2 = [1, 2, 3, ["a", "b", "c"], [2.5, 5.6, 3.9]];
+console.log(mixedArray2);
+console.log(mixedArray2.flat(Infinity));
---
+++ JavaScript/class-2/02-object.js
@@ -0,0 +1,46 @@
+// Object :
+
+// A collection of properties, where each property is defined as a key-value pair.
+// Each property has a key and a value.
+// The value can be a string, number, boolean, array, object, or function.
+
+// Syntax:
+
+// Traditional method
+
+let student = {
+  name: "Biswa",
+  age: 24,
+  city: "Jajpur",
+  marks: [80, 90, 70],
+  isMarried: false,
+  address: { po: "RJP", pin: 755009 },
+};
+// console.log(student);
+
+let array = ["Biswa", 24, "Jajpur", [80, 90, 70], false];
+
+
+// Constructor :
+
+let newStudent = new Object();
+newStudent.name = "Biswa";
+newStudent.age = 24;
+newStudent.city = "Jajpur";
+newStudent.marks = [80, 90, 70];
+newStudent.isMarried = false;
+newStudent.address = { po: "RJP", pin: 755009 };
+
+// console.log(newStudent);
+
+// Accessing object :
+
+// myarray[0]
+console.log(newStudent.name); 
+console.log(newStudent["marks"]); 
+console.log(newStudent.city);
+console.log(newStudent.isMarried);
+console.log(newStudent.address);
+console.log(newStudent.age);
+console.log(student.city);
+
